38ffd6fc49d7dc2bfbf3f64f2f19044d3b8df2af
lewissheaffer <lewiss@iastate.edu>
2019-12-14 00:51:00 -0600

    Finished base code for compressor and decompressor classes

--- src/mrZip/Compressor.java
+++ src/mrZip/Compressor.java
@@ -2,48 +2,81 @@ package mrZip;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Scanner;
-
-import com.googlecode.concurrenttrees.common.Iterables;
-import com.googlecode.concurrenttrees.common.PrettyPrinter;
-import com.googlecode.concurrenttrees.radix.ConcurrentRadixTree;
-import com.googlecode.concurrenttrees.radix.RadixTree;
-import com.googlecode.concurrenttrees.radix.node.concrete.DefaultCharArrayNodeFactory;
-import com.googlecode.concurrenttrees.radix.node.util.PrettyPrintable;
 
 public class Compressor {
 	public static void main(String[] args) {
-		RadixTree<Integer> tree = new ConcurrentRadixTree<Integer>(new DefaultCharArrayNodeFactory());
+		HashMap<String,String> map = new HashMap<String,String>();
 		Scanner fileInput = new Scanner(System.in);
-		System.out.println("Enter file address (/..filename.txt)");
+		System.out.println("Enter file address (filename.txt)");
 		String fileName = fileInput.next();
+		String dirFileName = "uncompressed/" + fileName;
 		fileInput.close();
-		File f = new File(fileName);
+		File f = new File(dirFileName);
 		Scanner fileScanner;
 		try {
+			PrintWriter writer = new PrintWriter(("compressed/" + fileName), "UTF-8");
 			fileScanner = new Scanner(f);
+			char segmentKeyChar1 = '"'; // First two characters in ascii at index 32
+			char segmentKeyChar2 = '"';
+			boolean newLine = false;
 			while(fileScanner.hasNextLine()) {
+				if (newLine) {
+					writer.print(System.getProperty("line.separator"));
+				}
+				newLine = true;
 				String line = fileScanner.nextLine();
 				char[] lineArray = line.toCharArray();
-				int keyCount = 0;
+				lineLoop:
 				for(int i = 0; i<lineArray.length; i++) {
-					String tempString = "" + lineArray[i];
-					while(tree.getValueForExactKey(tempString) != null) {
-						if(i+1 < lineArray.length) {
-							i++;
-							tempString += lineArray[i];
+					String segmentKey = "" + segmentKeyChar1 + segmentKeyChar2;
+					String tempString = "";
+					boolean reusedSegment = false;
+					char lastChar = lineArray[i];
+					while(map.containsKey(tempString + lastChar)) {
+						reusedSegment = true;
+						tempString += "" + lineArray[i];
+						//If the end of the line is reached
+						//and the current segment is in the map
+						if(i + 1 >= lineArray.length) {
+							writer.print("!" + map.get(tempString));
+							if ((int) segmentKeyChar2 == 127) {
+								segmentKeyChar2 = '"';
+								segmentKeyChar1++;
+							} else {
+								segmentKeyChar2++;
+							}
+							break lineLoop;
 						}
+						lastChar = lineArray[++i];
 					}
-					System.out.println(tempString);
-					tree.put(tempString, keyCount);
-					keyCount++;
+					if(!reusedSegment) {
+						writer.print("  " + lineArray[i]);
+					}
+					else {
+						writer.print(map.get(tempString) + lastChar);
+					}
+					map.put(tempString + lastChar, segmentKey);
+					if ((int) segmentKeyChar2 == 127) {
+						segmentKeyChar2 = '"';
+						segmentKeyChar1++;
+					} else {
+						segmentKeyChar2++;
+					}
 				}
-				System.out.println("Keys starting with 'H': " + Iterables.toString(tree.getKeysStartingWith("H")));
-				PrettyPrinter.prettyPrint((PrettyPrintable) tree, System.out);
 			}
+			writer.close();
 			fileScanner.close();
+			File compressedFile = new File("compressed/" + fileName);
+			System.out.println("Original File Size: " + f.length() + " Bytes\nCompressed File Size: " + compressedFile.length() + " Bytes\nRatio:" + ((100 * (double)compressedFile.length()/f.length()) + "%"));
 		} catch (FileNotFoundException e) {
 			System.out.println("No file found. Check for syntax errors.");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			throw new AssertionError("UTF-8 is unknown");
 		}
 	}
 }
--- src/mrZip/Decompressor.java
+++ src/mrZip/Decompressor.java
@@ -1,5 +1,98 @@
 package mrZip;
 
-public class Decompressor {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class Decompressor {
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		Scanner fileInput = new Scanner(System.in);
+		System.out.println("Decompressor");
+		System.out.println("Enter file address (filename.txt)");
+		String fileName = fileInput.next();
+		String dirFileName = "compressed/" + fileName;
+		fileInput.close();
+		File f = new File(dirFileName);
+		Scanner fileScanner;
+		try {
+			PrintWriter writer = new PrintWriter(("decompressed/" + fileName), "UTF-8");
+			fileScanner = new Scanner(f);
+			String encodedChar;
+			String addedChar;
+			char segmentKeyChar1 = '"'; // First two characters in ascii at index 32
+			char segmentKeyChar2 = '"';
+			char retrievalKeyChar1 = '"';
+			char retrievalKeyChar2 = '"';
+			boolean newLine = false;
+			while (fileScanner.hasNextLine()) {
+				int lineIndex = 0;
+				if(newLine) {
+					writer.print(System.getProperty("line.separator"));
+				}
+				newLine = true;
+				String line = fileScanner.nextLine();
+				char[] lineArray = line.toCharArray();
+				for (int i = 0; i < (lineArray.length) / 3; i++) {
+					// First key is assumed to start at "" and continue upward (example "#,"$,"%
+					// First Digit
+					char encodedChar1 = lineArray[lineIndex++];
+					encodedChar = "" + encodedChar1;
+					// Second Digit
+					char encodedChar2 = lineArray[lineIndex++];
+					encodedChar += encodedChar2;
+
+					// Third digit
+					// If new digit it found
+					if (encodedChar.equals("  ")) {
+						// Third Digit
+						addedChar = "" + lineArray[lineIndex++];
+					}
+					// ! As the first character of a segment
+					//represents a duplicate segment from a previous key.
+					else if (encodedChar1 == '!') {
+						addedChar = map.get("" + encodedChar2 + lineArray[lineIndex++]);
+					}
+					
+					// If encoded segment references a previous segment
+					else {
+						addedChar = "" + map.get(encodedChar) + lineArray[lineIndex++];
+					}
+					String segmentKey = "" + segmentKeyChar1 + segmentKeyChar2;
+					map.put(segmentKey, addedChar);
+
+					// Rollover statements for both segmentKeyChars
+					if ((int) segmentKeyChar2 == 127) {
+						segmentKeyChar2 = '"';
+						segmentKeyChar1++;
+					} else {
+						segmentKeyChar2++;
+					}
+					
+				}
+				while(!(retrievalKeyChar2 == segmentKeyChar2 && retrievalKeyChar1 == segmentKeyChar1)) {
+						String retrievalKey = "" + retrievalKeyChar1 + retrievalKeyChar2;
+						writer.print(map.get(retrievalKey));
+						if ((int) retrievalKeyChar2 == 127) {
+							retrievalKeyChar2 = '"';
+							retrievalKeyChar1++;
+						} else {
+							retrievalKeyChar2++;
+						}
+				}
+				retrievalKeyChar2 = segmentKeyChar2;
+				retrievalKeyChar1 = segmentKeyChar1;
+			}
+			writer.close();
+			fileScanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No file found. Check for syntax errors.");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			throw new AssertionError("UTF-8 is unknown");
+		}
+	}
 }
